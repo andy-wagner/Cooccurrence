0546ef618568f4871a028f6e12e9cfa74d4cba93
schen149 <schen149@illinois.edu>
2018-04-02 18:03:28 -0400

    save after dl reading group

--- src/main/java/org/cogcomp/nlp/statistics/cooccurrence/core/TermIDMapping.java
+++ src/main/java/org/cogcomp/nlp/statistics/cooccurrence/core/TermIDMapping.java
@@ -1,17 +1,23 @@
 package org.cogcomp.nlp.statistics.cooccurrence.core;
 
-import java.util.HashMap;
+import com.google.common.collect.HashBiMap;
 
 public class TermIDMapping{
-    public HashMap<String, Integer> termIDMap;
+    public HashBiMap<String, Integer> termIDMap;
 
     public TermIDMapping() {
-        termIDMap = new HashMap<>();
+        termIDMap = HashBiMap.create();
     }
 
     public void put(String term, Integer id) {
         termIDMap.put(term, id);
     }
 
-    public void
+    public void getIdFromTerm(String term) {
+        termIDMap.get(term);
+    }
+
+    public void getTermFromId(Integer id) {
+        
+    }
 }
