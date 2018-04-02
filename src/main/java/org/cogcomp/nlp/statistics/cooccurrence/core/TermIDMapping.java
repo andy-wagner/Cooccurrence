8618769d62d7aafcfce3efc27bc5be591d52acd3
schen149 <schen149@illinois.edu>
2018-04-02 18:50:32 -0400

    finished term id mapping

--- src/main/java/org/cogcomp/nlp/statistics/cooccurrence/core/TermIDMapping.java
+++ src/main/java/org/cogcomp/nlp/statistics/cooccurrence/core/TermIDMapping.java
@@ -1,8 +1,14 @@
 package org.cogcomp.nlp.statistics.cooccurrence.core;
 
 import com.google.common.collect.HashBiMap;
+import edu.illinois.cs.cogcomp.core.io.LineIO;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
 
 public class TermIDMapping{
+    
     public HashBiMap<String, Integer> termIDMap;
 
     public TermIDMapping() {
@@ -13,11 +19,29 @@ public class TermIDMapping{
         termIDMap.put(term, id);
     }
 
-    public void getIdFromTerm(String term) {
-        termIDMap.get(term);
+    public Integer getIdFromTerm(String term) {
+        return termIDMap.get(term);
     }
 
-    public void getTermFromId(Integer id) {
-        
+    public String getTermFromId(Integer id) {
+        return termIDMap.inverse().get(id);
     }
+
+    /**
+     * Load mapping from a CSV file
+     * The expected format of the csv file -- N lines, each line contains a String (term) and an integer (id)
+     */
+    public void readFromCSV(String csvpath, String delimiter) throws FileNotFoundException, IOException{
+        List<String> lines = LineIO.read(csvpath);
+        for (String line: lines) {
+            String[] parsed = line.split(delimiter);
+            if (parsed.length != 2)
+                continue;
+
+            Integer id = Integer.parseInt(parsed[1]);
+            termIDMap.put(parsed[0], id);
+        }
+    }
+
+
 }
